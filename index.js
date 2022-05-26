40b7c12a998266251377d53793d691a0b67f983f
caleb olusegun <olupeniel2000@gmail.com>
2022-05-26 15:59:31 +0100

    interactive rating component almost done_changes made to html and css

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+let buttons = document.querySelector('.btn');
+let ratingContainer = document.getElementsByClassName('.container');
+let thanksContainer = document.getElementsByClassName('.container2');
+let submit = document.querySelector('#submit-btn');
+let rating = document.querySelector('#rating')
 
+let choice = false;
+buttons.addEventListener("click",e =>{
+    if(e.target.matches('button')){
+        let chosenRating = e.target.innerHTML;
+        rating.textContent = chosenRating;
+        choice =true;
+    }
+})
 
-
-
-
-
-
+submit.addEventListener("click",e => {
+    if (choice){
+        ratingContainer.style.display = "none";
+        thanksContainer.style.display = "block"   
+     }
+})
 
 
 
